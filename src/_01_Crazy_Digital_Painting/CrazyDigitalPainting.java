d12704b08cc500e06f309a3a227b269d881394ad
Floyo <Floyo@DESKTOP-G71OOBP.home>
2020-06-17 17:06:02 -0700

    CrazyDigitalPainting done.

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,23 +1,32 @@
 package _01_Crazy_Digital_Painting;
 
-public class CrazyDigitalPainting {
-	//1. Create two final static integers for the width and height of the display.
+import java.awt.Color;
+import java.util.Random;
 
+public class CrazyDigitalPainting extends ColorArrayDisplayer{
+	//1. Create two final static integers for the width and height of the display.
+final static int width = 1000;
+final static int height = 600;
 	
-	//2. Create a 2D array of Color objects. You will need to import
+	static //2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
-	
+	Color[][] color = new Color[width][height];
 	
 	
 	public CrazyDigitalPainting() {
 		//3. Open the crazy_digital_painting.png file and look at the image.
-		
+		//okay
 		//4. Iterate through the 2D array and initialize each Color object
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
 		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-		
+		Random r = new Random();
+		for (int i = 0; i < color.length; i++) {
+			for (int j = 0; j < color[i].length; j++) {
+				color[i][j] = new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+			}
+		}
 		//5. Come up with your own pattern to make a cool crazy image.
 		
 		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
@@ -25,6 +34,7 @@ public class CrazyDigitalPainting {
 	}
 	
 	public static void main(String[] args) {
-		new CrazyDigitalPainting();
+		 new CrazyDigitalPainting();
+		 CrazyDigitalPainting.displayColorsAsImage(color);
 	}
 }
