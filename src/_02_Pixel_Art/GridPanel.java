0cb18146041e6f436d306d7af44a8ad36e680436
Floyo <Floyo@DESKTOP-G71OOBP.home>
2020-06-19 16:22:25 -0700

    Boxes not showing up correctly

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -17,7 +17,7 @@ public class GridPanel extends JPanel{
 	private int cols;
 	
 	//1. Create a 2D array of pixels. Do not initialize it yet.
-	
+	Pixel[][] p;
 	private Color color;
 	
 	public GridPanel(int w, int h, int r, int c) {
@@ -34,10 +34,14 @@ public class GridPanel extends JPanel{
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
 		
 		//2. Initialize the pixel array using the rows and cols variables.
-		
+		p = new Pixel[rows][cols];
 		
 		//3. Iterate through the array and initialize each element to a new pixel.
-		
+		for (int i = 0; i < p.length; i++) {
+			for (int j = 0; j < p[i].length; j++) {
+				p[i][j] = new Pixel(pixelWidth, pixelHeight);
+			}
+		}
 		
 	}
 	
@@ -48,6 +52,14 @@ public class GridPanel extends JPanel{
 	public void clickPixel(int mouseX, int mouseY) {
 		//5. Use the mouseX and mouseY variables to change the color
 		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		for (int i = 0; i < p.length; i++) {
+			for (int j = 0; j < p[i].length; j++) {
+				if(mouseX>=p[i][j].x&&mouseX<p[i][j].x+pixelWidth&&mouseY>=p[i][j].y&&mouseY<p[i][j].y+pixelHeight) {
+					//idk how to change color
+				}
+			}
+		}
+		
 	}
 	
 	@Override
@@ -55,6 +67,11 @@ public class GridPanel extends JPanel{
 		//4. Iterate through the array.
 		//   For every pixel in the list, fill in a rectangle using the pixel's color.
 		//   Then, use drawRect to add a grid pattern to your display.
-		
+		for (int i = 0; i < p.length; i++) {
+			for (int j = 0; j < p[i].length; j++) {
+				g.fillRect(i, j, pixelWidth, pixelHeight);
+				g.drawRect(i, j, pixelWidth, pixelHeight);
+			}
+		}
 	}
 }
