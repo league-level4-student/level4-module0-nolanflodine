b70c7d9ad9144de721d7369ec0f8b7280b8ed87f
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-06-29 19:16:24 -0700

    Maze Maker

--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -45,28 +45,34 @@ public class MazeMaker extends Maze{
 		//B. Get an ArrayList of unvisited neighbors using the current cell and the method below
 		
  		//C. if has unvisited neighbors,
-		if(!currentCell.hasBeenVisited()) { //I know this line is wrong but idk how to identify the neighbors
+		//ArrayList<Cell> n = getUnvisitedNeighbors(currentCell);
+		if(getUnvisitedNeighbors(currentCell).size()>0) {
 			//C1. select one at random.
-			
+			r1 = randGen.nextInt(getUnvisitedNeighbors(currentCell).size());
+			r2 = randGen.nextInt(getUnvisitedNeighbors(currentCell).size());
 			//C2. push it to the stack
-		
+		uncheckedCells.push(c[r1][r2]);
 			//C3. remove the wall between the two cells
-
+			removeWalls(c[r1][r2], currentCell);
 			//C4. make the new cell the current cell and mark it as visited
-		
+		currentCell = c[r1][r2];
+		currentCell.hasBeenVisited();
 			//C5. call the selectNextPath method with the current cell
+		selectNextPath(currentCell);
 		}
 			
 		//D. if all neighbors are visited
-		
+		if(getUnvisitedNeighbors(currentCell).size()==0) {
 			//D1. if the stack is not empty
-			
+			if(uncheckedCells.size()>0) {
 				// D1a. pop a cell from the stack
-		
+					Cell p = uncheckedCells.pop();
 				// D1b. make that the current cell
-		
+					currentCell = p;
 				// D1c. call the selectNextPath method with the current cell
-				
+					selectNextPath(p);
+			}
+		}
 			
 		
 	}
@@ -75,13 +81,14 @@ public class MazeMaker extends Maze{
 	//   This method will check if c1 and c2 are adjacent.
 	//   If they are, the walls between them are removed.
 	private static void removeWalls(Cell c1, Cell c2) {
-		
+		if(c1.getX()+100==c2.getX()||c1.getX()-100==c2.getX()||c1.getY()+100==c2.getY()||c1.getY()-100==c2.getY()) { // checks if they are adjacent I think
+		}
 	}
 	
 	//8. Complete the getUnvisitedNeighbors method
 	//   Any unvisited neighbor of the passed in cell gets added
 	//   to the ArrayList
-	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {
+	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {  
 		return null;
 	}
 }
